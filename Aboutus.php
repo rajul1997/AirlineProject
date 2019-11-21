0a8aa7feeb202a62ef701ca9ca77cf212820718e
rajul soni <rajulsoni17@gmail.com>
2019-11-21 22:37:06 +0530

    Create Aboutus.php
    this is airline

---
+++ Aboutus.php
@@ -0,0 +1,87 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Airline Aboutus</title>
+        <link href="Style/Aboutus.css" rel="stylesheet">
+        
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        <style>
+            body
+            {
+                margin: 0px;
+                padding: 0px;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+        <div class="Top">
+            <div class="logo">
+               <h1 style= "font-size:20px;padding-left: 10px;"><i class="fas fa-plane-departure"></i>&nbsp; BONBOYZ</h1>
+            </div>
+            <ul class="Register">
+                <li class="Registermenu"><a href="">About Us&nbsp;&nbsp;<i class="fas fa-plane-departure"></i></a></li>
+                <li class="Registermenu1"><a href="Log_in.php">Log In&nbsp;<i class="fas fa-sign-in-alt"></i></a></li>
+                <li class="Registermenu2"><a href="Registration.php"><i class="fas fa-user"></i>&nbsp;Create Account</a></li>
+                <li class="Registermenu3"><a href="Profile.php"><i class="fas fa-user-tie"></i>&nbsp;My Profile</a></li>
+            </ul> 
+            </div>
+       
+            <div class="pagesmenu">
+            <ul class="topmenus">
+                <li class="topmenu"><a href="Home.php">Home</a></li>
+                <li class="topmenu"><a href="Flights.php">Flights</a></li>
+                <li class="topmenu"><a href="Holidays.php">Holidays</a></li>
+                <li class="topmenu"><a href="Xplore.php">X-Plore</a></li>
+                <li class="topmenu"><a href="Luxory.php">Luxury Travels</a></li>
+            </ul> 
+        </div> 
+            </header>
+        
+        
+  <div class="airoplan">
+                
+            </div>
+            <hr>
+            <div class="firsttitle">
+                <p style="font-size: 40px;">About Us</p>
+            </div>
+            <div class="aboutusdetail">
+            <div class="detail">
+                <pre>
+  BonVoyage:Airline Reservation System is a online booking system which 
+                   
+  provides various facilities like booking of flights ,booking of tours 
+
+  in all places in India in a cheap rate.It is an Indian travel search site,
+
+  a place where people are inspired to plan and book direct from millions of
+
+  travel options at the best prices.We are unbiased and free, which means
+ 
+  that the people who use us every month can trust our comprehensive range
+
+  of flight,luxurious tour package options.
+                    
+                </pre>
+            </div>
+            <div class="rightcon">
+                <pre>
+  Website created by:
+                         
+    *Rajul Soni 
+                        
+    *Niki Behera
+                        
+    *Jitesh sahu
+
+
+
+      -(MCA-4th SEM)
+                </pre>
+            </div>
+            </div>
+            <footer><?php include 'Footer.php';?></footer>
+    </body>
+</html>
+
